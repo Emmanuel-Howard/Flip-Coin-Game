139bdf36c983268a001f5f28e5ac520fcaad5052
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-03-07 20:23:26 -0500

    flipBtn on click animation in js

--- index.js
+++ index.js
@@ -4,3 +4,21 @@ let tails = 0;
 let coin = document.querySelector(".coin")
 let flipBtn = document.querySelector("#flipBtn")
 let resetBtn = document.querySelector("#resetBtn")
+
+flipBtn.addEventListener("click", ()=>{
+    let i = Math.floor(Math.random() * 2);
+    coin.computedStyleMap.animation = "none";
+    if(i){
+        setTimeout(function(){
+            coin.style.animation = "spin-heads 3s forwards";
+        }, 100);
+        heads++;
+    }else{
+        setTimeout(function(){
+            coin.style.animation = "spin-tails 3s forwards";
+        }, 100);
+        tails++;
+    }
+    setTimeout(updateStats, 3000);
+    disableButton();
+});
