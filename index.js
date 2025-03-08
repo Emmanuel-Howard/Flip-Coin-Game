c5ca66ff5b9ab0d12569f6277a10e5120a2d66c9
Emmanuel Howard <emmanuelhoward3000@gmail.com>
2025-03-07 20:15:32 -0500

    Defining constants & Variables

---
+++ index.js
@@ -0,0 +1,6 @@
+// Defining constants & variables 
+let heads = 0;
+let tails = 0;
+let coin = document.querySelector(".coin")
+let flipBtn = document.querySelector("#flipBtn")
+let resetBtn = document.querySelector("#resetBtn")
